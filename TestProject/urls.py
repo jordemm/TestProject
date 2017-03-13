3b4e76748d795d32ab37b88dfceec803f61076aa
jordemm <developertestmail24@gmail.com>
2017-03-13 13:22:38 +0100

    changed project directories

--- TestProject/urls.py
+++ TestProject/urls.py
@@ -17,11 +17,13 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from TestProject.apps.portaAperta.views import index
 
 urlpatterns = [
+    url(r'^$', index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#
+# if settings.DEBUG:
+# 	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 	# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
